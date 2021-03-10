3c364ea2fcec904045c6fee45aedb076d83b3b28
Erimi <erikamichu13@hotmail.com>
2021-03-09 19:23:28 -0500

    primera parte del reto 0.1

--- placas.php
+++ placas.php
@@ -1,10 +1,10 @@
 <?php
 $matricula =$_POST['matricula'];
 
-
+if(!empty($matricula)){
     $host = "loacalhost";
     $dbusername ="root";
     $dbpassword ="";
     $dbname="cliente";
+     }
     ?>
-    
